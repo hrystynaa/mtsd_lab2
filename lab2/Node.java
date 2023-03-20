b3300fbc908e828ccb104925d331ba9565ca0251
hrystynaa <yaskulskahrystyna@gmail.com>
2023-03-20 19:29:02 +0200

    Implement methods for list class

--- lab2/Node.java
+++ lab2/Node.java
@@ -1,27 +1,11 @@
 package lab2;
 
 public class Node {
-    private char data;
-    private Node next;
+    protected char data;
+    protected Node next;
 
     public Node(char data) {
         this.data = data;
         this.next = null;
-    }
-
-    public char getData() {
-        return data;
-    }
-
-    public void setData(char data) {
-        this.data = data;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
     }
 }

defe7eac6cfd7a11172ec2f9ad11b9c93ef92a89
hrystynaa <yaskulskahrystyna@gmail.com>
2023-03-20 16:21:55 +0200

    Implement circular linked list class

---
+++ lab2/CircularLinkedList.java
@@ -0,0 +1,12 @@
+package lab2;
+
+public class CircularLinkedList {
+    private Node head;
+    private Node tail;
+
+    public CircularLinkedList() {
+        head = null;
+        tail = null;
+    }
+
+}
